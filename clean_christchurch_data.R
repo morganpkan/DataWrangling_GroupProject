e888e419ddd4c72c9c6e30c8c9a1d4a79ae50db2
Morgan <morganpkan@gmail.comm>
2026-09-15 17:59:25 +1200

    Deduplication added to cleaning filters- Previously was in dataset compiler

--- clean_christchurch_data.R
+++ clean_christchurch_data.R
@@ -1,11 +1,22 @@
 library(tidyverse)
-christchurch <- read_csv('christchurch.csv')
+christchurch_raw <- read_csv('christchurch.csv')
 
 
 #Cleaning
 
 #NOTE: Price is NA from Dec 2025, Jan 2026, and Feb 2026
 #This is an error from airbnb raw data.
+
+
+#Deduplicate data using april listings as most recent
+#April listings used as most recent tenancy/rental bond data in April 2026
+#May and June 2026 data excluded from this dataset.
+christchurch <- christchurch_raw |>
+  filter(scrape_date <= as_date("2026-04-30")) |>
+  group_by(id) |>
+  slice_max(scrape_date, n = 1, with_ties = FALSE) |>
+  ungroup() |>
+  mutate(days_since_review = as.numeric(scrape_date - as_date(last_review)))
 
 #Removed license column as it was entirely NA
 christchurch <- christchurch |> select(-license)
