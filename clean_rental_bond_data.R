48dc2efd6142738e0e0472e8335296c046bbc322
Morgan <morganpkan@gmail.comm>
2026-09-15 19:29:52 +1200

    Filters for rental bond dataset

---
+++ clean_rental_bond_data.R
@@ -0,0 +1,24 @@
+library(tidyverse)
+library(lubridate)
+
+tenancy_raw <- read_csv('Detailed-Quarterly-Tenancy-Q1-2020-Q3-2026.csv')
+
+
+#Time frame set to match christchurch airbnb dataset. Timeframe 2025-10-01 - 2026-04-01
+tenancy <- tenancy_raw |>
+  filter(TimeFrame >= as_date("2025-10-01"),
+         TimeFrame <= as_date("2026-04-30"))
+
+
+#Location ID = '-99' or = 'NULL' yet removed because havent worked out what they mean
+
+#Changed 'Location Id' column from char to int. 'NULL' became NA
+tenancy <- tenancy |> mutate(`Location Id` = as.integer(`Location Id`))
+
+#Changed Median, Geometric Mean, Upper Quartile, and Lower Quartile rents from char to numeric
+tenancy <- tenancy |>
+  mutate(across(c(`Median Rent`, `Geometric Mean Rent`, `Upper Quartile Rent`, `Lower Quartile Rent`),
+                as.numeric))
+
+#Changed Log Std Dev Weekly Rent from char to numeric.
+tenancy <- tenancy |> mutate(`Log Std Dev Weekly Rent` = as.numeric(`Log Std Dev Weekly Rent`))
